e839ad7be8a3b9d83cc91271b29d93183c1b0e52
abspen1 <abspencer2097@gmail.com>
2020-08-12 13:40:18 -0700

    testing

--- source/add-email/handler.go
+++ source/add-email/handler.go
@@ -2,55 +2,53 @@ package function
 
 import (
 	"io/ioutil"
-	"log"
 	"net"
 	"net/http"
 	"regexp"
 	"strings"
-
-	"github.com/gomodule/redigo/redis"
 )
 
 var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
+// Handle function
 func Handle(w http.ResponseWriter, r *http.Request) {
-	var email []byte
+	// var email []byte
 	var name []byte
 
 	if r.Body != nil {
 		defer r.Body.Close()
 
-		body, _ := ioutil.ReadAll(r.Body.info.name)
+		body, _ := ioutil.ReadAll(r.Body)
 		name = body
-		body, _ = ioutil.ReadAll(r.Body.info.name)
-		email = body
+		// body, _ = ioutil.ReadAll(r.Body)
+		// email = body
 	}
 
 	// Check if email is valid
-	if e := string(email); !isEmailValid(e) {
-		// http code 400 is for a bad request
-		w.WriteHeader(http.Error(w, "Email not valid", 400))
-	} 
-	// Make sure name isn't empty
-	else if string(name) == "" {
-		w.WriteHeader(http.Error(w, "Enter your name", 400))
-	} 
-	else {
-		secret, _ := getDBSecret("redis-password")
-		c, err := redis.Dial("tcp", "192.168.1.6:6379")
-		if err != nil {
-			log.Fatal(err)
-		}
-		response, err := c.Do("AUTH", secret)
-		if err != nil {
-			log.Fatal(err)
-		}
+	// if e := string(email); !isEmailValid(e) {
+	// 	w.Write([]byte("Invalid email"))
+	// } else if string(name) == "" {
+	// 	w.Write([]byte("Enter your name"))
+	// } else {
+	// 	secret, _ := getDBSecret("redis-password")
+	// 	c, err := redis.Dial("tcp", "192.168.1.6:6379")
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	response, err := c.Do("AUTH", secret)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Println("Connected!", response)
 
-		c.Do("HSET", "emails", string(email), string(name))
-		defer c.Close()
+	// 	c.Do("HSET", "emails", string(email), string(name))
+	// 	defer c.Close()
 
-		w.WriteHeader(http.StatusOK)
-	}
+	// 	w.WriteHeader(http.StatusOK)
+	// }
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(name))
+
 }
 
 func isEmailValid(e string) bool {
@@ -66,4 +64,15 @@ func isEmailValid(e string) bool {
 		return false
 	}
 	return true
+}
+
+func getDBSecret(secretName string) (secretBytes []byte, err error) {
+	// read from the openfaas secrets folder
+	secretBytes, err = ioutil.ReadFile("/var/openfaas/secrets/" + secretName)
+	if err != nil {
+		// read from the original location for backwards compatibility with openfaas <= 0.8.2
+		secretBytes, err = ioutil.ReadFile("/run/secrets/" + secretName)
+	}
+
+	return secretBytes, err
 }
