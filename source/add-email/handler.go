a12c18a70a60e18e8ad419ad644dbe4339123909
Ryan Gulden <rgulden@live.com>
2020-08-12 22:01:50 -0500

    added my solution

--- source/add-email/handler.go
+++ source/add-email/handler.go
@@ -35,25 +35,42 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 		body, _ := ioutil.ReadAll(r.Body)
 		info = body
 	}
-	
+
 	var requestBody Body
-	
-	requestBody = json.Unmarshall(info, &requestBody)
 
-	// Creating the maps for JSON
-	m := map[string]interface{}{}
+	_ = json.Unmarshal(info, &requestBody)
 
-	// Parsing/Unmarshalling JSON encoding/json
-	_ = json.Unmarshal(info, &m)
-	in.parseMap(m)
+	fmt.Printf("%s", requestBody.info.Name)
 
 	// Check if email is valid
-	if e := string(in.Email); !isEmailValid(e) {
+	if !isEmailValid(requestBody.info.Email) {
 		w.Write([]byte("Invalid email address"))
-	} else if in.Name == "" {
+	} else if requestBody.info.Name == "" {
 		w.Write([]byte("Enter your name"))
 	} else {
-		in.success(w)
+
+		secret, _ := getDBSecret("redis-password")
+		c, err := redis.Dial("tcp", "192.168.1.6:6379")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = c.Do("AUTH", secret)
+		if err != nil {
+			log.Fatal(err)
+		}
+		exists, _ := redis.Bool(c.Do("EXISTS", "emails", in.Email))
+		if exists {
+			w.Write([]byte("You're already signed up for emails :)"))
+		} else {
+			fmt.Println("Exists works with hashes")
+			c.Do("HSET", "emails", in.Email, in.Name)
+			message := fmt.Sprintf("Added %s: %s to database", in.Name, in.Email)
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(message))
+		}
+		defer c.Close()
+
 	}
 }
 
@@ -72,22 +89,6 @@ func isEmailValid(e string) bool {
 	return true
 }
 
-func (in *Info) parseMap(aMap map[string]interface{}) {
-	for key, val := range aMap {
-		switch concreteVal := val.(type) {
-		case map[string]interface{}:
-			in.parseMap(val.(map[string]interface{}))
-		default:
-			// fmt.Println(key, ":", concreteVal)
-			if key == "name" {
-				in.Name = concreteVal.(string)
-			} else if key == "email" {
-				in.Email = concreteVal.(string)
-			}
-		}
-	}
-}
-
 func getDBSecret(secretName string) (secretBytes []byte, err error) {
 	// read from the openfaas secrets folder
 	secretBytes, err = ioutil.ReadFile("/var/openfaas/secrets/" + secretName)
@@ -96,28 +97,4 @@ func getDBSecret(secretName string) (secretBytes []byte, err error) {
 		secretBytes, err = ioutil.ReadFile("/run/secrets/" + secretName)
 	}
 	return secretBytes, err
-}
-
-func (in Info) success(w http.ResponseWriter) {
-	secret, _ := getDBSecret("redis-password")
-	c, err := redis.Dial("tcp", "192.168.1.6:6379")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = c.Do("AUTH", secret)
-	if err != nil {
-		log.Fatal(err)
-	}
-	exists, _ := redis.Bool(c.Do("EXISTS", "emails", in.Email))
-	if exists {
-		w.Write([]byte("You're already signed up for emails :)"))
-	} else {
-		fmt.Println("Exists works with hashes")
-		c.Do("HSET", "emails", in.Email, in.Name)
-		message := fmt.Sprintf("Added %s: %s to database", in.Name, in.Email)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(message))
-	}
-	defer c.Close()
 }
